6c11a4a2cdd45da0a7be8514be541b1095b2c481
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-07-15 14:05:38 -0300

    feat: global exception handler

---
+++ src/main/java/com/api/mywallet/exception/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.api.mywallet.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+
+        e.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return new ResponseEntity<Map<String, String>>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<String> handleResourceUserAlreadyExistsException(UserAlreadyExistsException e) {
+        return new ResponseEntity<>("User already exists", HttpStatus.FORBIDDEN);
+    }
+
+}
